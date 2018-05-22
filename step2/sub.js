6079c4af94fd7007afe040fcb7c4091b2c90a283
yuri <lily0114@gmail.com>
2018-05-22 13:17:07 +0900

    var.0.4 わからないところ解消

--- step2/sub.js
+++ step2/sub.js
@@ -1,69 +1,64 @@
-window.onload = function(){
-  var x=10, y = 10;
-  var cv = document.getElementById('cv');
-  var ctx = cv.getContext('2d');
+window.onload = function(){ //全部読み込んでから実行する
+  var xCoor = 0, yCoor = 0; //座標のスタート位置
+  var cv = document.getElementById('cv');　//htmlのcanvasタグのID取得して動かせるようにする
+  var ctx = cv.getContext('2d');　//canvasに描画するためのAPIにアクセスできるようにする
 
-
-  var wd_width = window.innerWidth;
-  var wd_height = window.innerHeight;
-  ctx.canvas.width  = wd_width;
-  ctx.canvas.height = wd_height;
-
-     function star(x,y){
-        ctx.fillStyle = '#f0e68c';
-        ctx.strokeStyle = '#f0e68c';
-
-        ctx.beginPath();
-        ctx.moveTo(10+x,20+y);
-        ctx.lineTo(50+x, 20+y);
-        ctx.lineTo(17+x, 42+y);
-        ctx.lineTo(30+x,6+y);
-        ctx.lineTo(43+x, 42+y);
-        ctx.lineTo(10+x,20+y);
-        ctx.fill();
-    }
-
-    var flag = false;
-    var yoko;
-    var tate;
-    function onClick(e) {
-
-      console.log("click");
-      var x = e.clientX - cv.offsetLeft;
-      var y = e.clientY - cv.offsetTop;
-      console.log("x:", x, "y:", y);
-      flag = true;
-      yoko = x;
-      tate = y;
-  }
-  
-  cv.addEventListener('click', onClick, false);
-
-setInterval(function(){
-  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-  star(x,y);
-  ctx.fillStyle="#4682b4";
-  x++; y++;
-  //console.log(x,y);
-  if (flag==true){
-    star(yoko,tate);
+  var wd_width = window.innerWidth; //横の画面サイズ取得
+  var wd_height = window.innerHeight;　//縦の画面サイズ取得
+  console.log(window.innerWidth, window.innerHeight)
+  cv.width  = wd_width;　//横の画面サイズをctxに代入 画面サイズに合わせた大きさにする
+  cv.height = wd_height;//縦の画面サイズをctxに代入
+  if(ctx.canvas == cv){
+    console.log('同じ');
   }
 
-  if (y>600){
-    x=0,y=0;
+  function star(xCoor,yCoor){　//☆の造形まとめた関数
+    ctx.fillStyle = '#f0e68c';　//☆の色
+    ctx.strokeStyle = '#f0e68c';　//☆の線の色
+
+    ctx.beginPath();　//いまのパスをいったんリセット
+    ctx.moveTo(10+xCoor,20+yCoor);　//線の開始位置セット
+    ctx.lineTo(50+xCoor, 20+yCoor);　//線を引く
+    ctx.lineTo(17+xCoor, 42+yCoor);　//線を引く
+    ctx.lineTo(30+xCoor,6+yCoor);　　//線を引く
+    ctx.lineTo(43+xCoor, 42+yCoor);　//線を引く
+    ctx.lineTo(10+xCoor,20+yCoor);　//線を引く
+    ctx.fill();　//塗りつぶし
+  }　//star関数終わり
+
+  var flag = false;　//マウスクリックイベントのフラグの箱を用意
+  console.log(flag);
+  var sideX; //　横
+  var lengthY; //　縦
+  function onClick(e) { //オンクリックイベント関数
+
+      console.log("click");　//クリックをconsoleに表示
+      var x = e.clientX - cv.offsetLeft;　//x座標の水平座標‐
+      var y = e.clientY - cv.offsetTop;　//y座標の垂直座標‐
+      console.log(cv.offsetLeft,cv.offsetTop);
+      console.log("x:", x, "y:", y);//座標をconsoleに表示
+      flag = true;　//フラグを立てる
+      sideX = x;　　//xの座標を代入
+      lengthY = y;　//yの座標を代入
   }
-},100);
 
+  cv.addEventListener('click', onClick, false);　//クリックイベントを用意
 
+  setInterval(function(){　//一定時間ごとに実行する　ループ　関数
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);//描画をクリアにする
+    star(xCoor,yCoor);　//☆の座標をセットして関数実行すr
+    xCoor++; yCoor++;　//座標を1ずつ足して右下へ動かしてく
+    //console.log(x,y);
+      if (flag==true){　//もしフラグがtrueなら
+        star(sideX,lengthY);　//クリックしたときstar関数を実行
+      }
 
+      if (yCoor>600){　//
+        xCoor=0,yCoor=0;
+        }
+    },100);
 
-
-
-
-
-
-
-};
+  };
 
 
 
